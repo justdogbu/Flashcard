a2e0f641a9948f631305b5117b07fa3c7c914c88
justdogbu <giaminhro1611@gmail.com>
2023-12-23 14:39:24 +0700

    selected image

--- app/src/main/java/com/example/flashcard/utils/Constant.java
+++ app/src/main/java/com/example/flashcard/utils/Constant.java
@@ -3,4 +3,7 @@ package com.example.flashcard.utils;
 public class Constant {
     public static final String USER_DATA = "USERDATA";
     public static final String SHARE_PREF = "SHAREDPREFKEY";
+    public static final int PICK_IMAGE_REQUEST = 111;
+    public static final int PICK_IMAGE_INTENT = 222;
+
 }

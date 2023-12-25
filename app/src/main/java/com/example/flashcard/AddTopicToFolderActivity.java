dbe372b4961f06b7eecf8f0db40742869dae5c86
justdogbu <giaminhro1611@gmail.com>
2023-12-25 18:57:13 +0700

    delete topic

--- app/src/main/java/com/example/flashcard/AddTopicToFolderActivity.java
+++ app/src/main/java/com/example/flashcard/AddTopicToFolderActivity.java
@@ -1,14 +1,23 @@
 package com.example.flashcard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AddTopicToFolderActivity extends AppCompatActivity {
 
+    private ImageButton returnBtn;
+    private ImageButton acceptTopicBtn;
+    private RecyclerView chosenTopicRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_topic_to_folder);
+
+        returnBtn = findViewById(R.id.returnBtn);
+        acceptTopicBtn = findViewById(R.id.acceptTopicBtn);
+        chosenTopicRecyclerView = findViewById(R.id.chosenTopicRecyclerView);
     }
 }
